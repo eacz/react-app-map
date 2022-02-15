72b72c90b902e91cb9d42df69d9e4de46ad79210
Esteban Canteros <estebanacanteros@gmail.com>
2022-02-14 23:54:43 -0300

    displaying the map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,15 +1,34 @@
-import { useContext } from 'react'
+import { useContext, useLayoutEffect, useRef } from 'react'
+import { Map } from 'mapbox-gl'
+
 import PlacesContext from '../context/places/PlacesContext'
 import { Loading } from './'
 
 const MapView = () => {
   const { userLocation, isLoading } = useContext(PlacesContext)
+  const mapDiv = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new Map({
+        container: mapDiv.current!,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: userLocation,
+        zoom: 15,
+      })
+    }
+  }, [isLoading, userLocation])
 
   if (isLoading) {
     return <Loading />
   }
 
-  return <div>{userLocation?.join(', ')}</div>
+  return (
+    <div
+      ref={mapDiv}
+      style={{ backgroundColor: 'red', height: '100vh', width: '100vw', position: 'fixed', top: 0, left: 0 }}
+    ></div>
+  )
 }
 
 export default MapView
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MapsApp from './MapsApp';
 import './globalStyles.css'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken=process.env.REACT_APP_MAPBOX_TOKEN || ''
+
 
 if(!navigator.geolocation){
   alert("Your browser doesn't have a geolocation option")
